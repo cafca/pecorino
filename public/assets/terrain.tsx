4f9655c294f9d452e74e85bfa969d40fa5fecd55
cafca <cafca@001.land>
2025-05-17 18:46:00 +0200

    Add assets

---
+++ public/assets/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="terrain" tilewidth="32" tileheight="32" tilecount="160" columns="10">
+ <image source="../assets/sprites/terrain_tiles_v2.png" width="320" height="512"/>
+</tileset>
